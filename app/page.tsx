2e00e97d5ca8b0591d662fef89daa00052e21e26
unknown <febinanto61@gmail.com>
2026-01-04 11:42:09 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ExploreBtn from "@/components/ExploreBtn";
+import events from "@/lib/constants";
+import EventCard from "@/components/EventCard";
+
+const Home = () => {
+    return (
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br/> Event you can't miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+            <ExploreBtn/>
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
+
+                <ul className="events">
+                    {events && events.length > 0 && events.map((event: IEvent) => (
+                        <li key={event.title} className="list-none">
+                            <EventCard {...event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>    )
+}
+export default Home
